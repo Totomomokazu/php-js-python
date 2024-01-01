f9ff6adb31d687780d9416e0e99d452074e68e4b
Totomomokazu <kanketomokazu@gmail.com>
2024-01-01 21:37:51 +0900

    トランプゲームの追加

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <title>データ登録</title>
 </head>
 <body>
+    <script src="test.js"></script>
+    <button><a href="index-tramp.html">trampで遊ぶ</a></button>
+    <button><a href="/tramp/index-tramp.html">trampで遊ぶ</a></button>
+    <button><a href="/gs_code/02_homework/php-js-python/tramp/index-tramp.html">trampで遊ぶ</a></button>
+    <!-- ↑ファイル構造を変える場合、相対パスで指定する必要があるため、htdocs以下のパスを指定する必要がある-->
     <button><a href="select.php">データベースを確認する</a></button>
     <form method="post" action="insert.php">
         <fieldset>

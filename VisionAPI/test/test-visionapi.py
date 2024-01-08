b31f20e951778efc1e9c11fbe76035d89594f32a
Totomomokazu <kanketomokazu@gmail.com>
2024-01-08 15:18:03 +0900

    pythonファイルの修正

--- VisionAPI/test/test-visionapi.py
+++ VisionAPI/test/test-visionapi.py
@@ -1,10 +1,32 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+import json
 import sys
-import some_image_processing_library
+
 
 # コマンドライン引数から画像ファイルのパスを取得
 image_path = sys.argv[1]
 
 # 画像処理
+subscription_key = "xx"
+endpoint = "xx"
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+def get_tags(filepath):
+    local_image = open(filepath, "rb")
+    tags_result_local = computervision_client.tag_image_in_stream(local_image)
+    tags = tags_result_local.tags
+    tag_names = [tag.name for tag in tags]
+    return tag_names
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    tags = get_tags(file_path)
+    print(json.dumps(tags))
+
+
 processed_image = some_image_processing_library.process_image(image_path)
 
 # 結果を出力

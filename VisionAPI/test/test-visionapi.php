0b266d8ea28646dfb23a0daaba32f53a1880b082
Totomomokazu <kanketomokazu@gmail.com>
2024-01-08 15:10:05 +0900

    phpファイルの修正

--- VisionAPI/test/test-visionapi.php
+++ VisionAPI/test/test-visionapi.php
@@ -2,7 +2,7 @@
 // PHPスクリプトの開始を宣言
 
 // 画像ファイルのパスを変数に代入
-$imagePath = 'image.jpg';
+$imagePath = 'sample1.png';
 
 // Pythonスクリプトを実行し、その結果を変数に代入
 // "python"はPythonスクリプトを実行するコマンド
@@ -12,4 +12,6 @@ $result = shell_exec("python test-visionapi.py " . escapeshellarg($imagePath));
 
 // Pythonスクリプトの実行結果を出力
 echo $result;
+echo "<br>";
+echo "test";
 ?>
